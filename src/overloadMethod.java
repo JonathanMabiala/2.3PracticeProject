bde5ed84db5b6979123f92da7b9e0a3813d3e031
Jonathan Mabiala <jonathan.mabiala@outlook.com>
2021-06-16 23:50:58 +0000

    Initial version

---
+++ src/overloadMethod.java
@@ -0,0 +1,20 @@
+
+public class overloadMethod {
+	
+	public void area(int b, int h) {
+		System.out.println("Area of Triangle: "+(0.5*h*b));
+	}
+	public void area(int r) {
+		System.out.println("Area of Circle: "+ (3.14*r*r));
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		overloadMethod ob = new overloadMethod();
+		ob.area(10,12);
+		ob.area(5);
+		
+	}
+
+}
